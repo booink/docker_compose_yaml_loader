96612d457d580b7a4c228d192d27289ca8d21a3e
booink <booink.work@gmail.com>
2018-11-22 13:53:15 +0900

    initial commit

---
+++ lib/docker_compose_yaml_loader.rb
@@ -0,0 +1,6 @@
+require "docker_compose_yaml_loader/version"
+
+module DockerComposeYamlLoader
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/docker_compose_yaml_loader/version.rb
@@ -0,0 +1,3 @@
+module DockerComposeYamlLoader
+  VERSION = "0.1.0"
+end
